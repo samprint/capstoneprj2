f83088915becb4c7d00e64aabb328939b750176b
samprint <simo1print@gmail.com>
2023-09-14 15:11:14 +0900

    Setting up the app components - week2 - 20

--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -1,6 +1,23 @@
+import combination from "../images/Combination.png"
+
 const Chicago = () => {
     return(
-        <div>Chicago</div>
+        <div className="chicago">
+            <div className="content">
+                <div className="text">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>
+                        We are a family owned Mediterranean restaurant, 
+                        focused on traditional recipe served with  
+                        a modern twist.
+                    </p>
+                </div>
+                <div>
+                    <img src={combination} alt="Chefs Pictures" />
+                </div>
+            </div>
+        </div>
     );
 
 };
