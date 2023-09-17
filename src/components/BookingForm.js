5697cd7210dc6d12e5a1878f428cfe3dbdbeec49
samprint <simo1print@gmail.com>
2023-09-17 13:54:03 +0900

    Form - week3 - 28

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,27 +1,73 @@
+import {useState} from "react";
+
 
 
 const BookingForm = () => {
+
+    const [date, setDate] = useState("2023-01-01");
+    const [time, setTime] = useState("");
+    const [guestsNumber, setGuestsNumber] = useState(1);
+
+    const availableTimes = [
+        "07:00",
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00",
+        "22:00",
+    ];
+
+    const clearForm = () => { 
+      };     
+
+    const handleSubmit = (e) => { 
+        e.preventDefault(); 
+        alert("Account created!"); 
+        clearForm(); 
+      }; 
+
     return(
         <div className="bookingform">
             <div className="form">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="datetime">
                         <div className="date">
                             <fieldset>
                                 <label for="res-date" >Choose date</label>
-                                <input type="date" id="res-date" className="input-box"/>
+                                <input 
+                                    type="date" 
+                                    id="res-date" 
+                                    className="input-box"
+                                    value={date}
+                                    onChange={(e) =>{
+                                        setDate(e.target.value)
+                                    }}
+                                />
                             </fieldset>
                         </div>
                         <div className="time">
                             <fieldset>
                                 <label for="res-time">Choose time</label>
                                 <select id="res-time" className="input-box">
-                                    <option>17:00</option>
+                                    {availableTimes.map((option) => (
+                                        <option>{option}</option>
+                                    ))}
+                                    {/* <option>17:00</option>
                                     <option>18:00</option>
                                     <option>19:00</option>
                                     <option>20:00</option>
                                     <option>21:00</option>
-                                    <option>22:00</option>
+                                    <option>22:00</option> */}
                                 </select>
                             </fieldset>
                         </div>
@@ -36,6 +82,10 @@ const BookingForm = () => {
                                 max="10" 
                                 id="guests"
                                 className="input-box"
+                                value={guestsNumber}
+                                onChange={(e) =>{
+                                    setGuestsNumber(e.target.value)
+                                }}
                             />
                         </fieldset>
                     </div>
