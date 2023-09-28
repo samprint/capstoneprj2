94eb3a2a9dba71f06a47351386363b19c6abb3a4
samprint <simo1print@gmail.com>
2023-09-28 11:34:20 +0900

    Starting unit test - week3 - 88
    some cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,62 +1,12 @@
 import { render, screen, within } from '@testing-library/react';
-import BookingForm from './components/BookingForm';
 import { BrowserRouter } from 'react-router-dom';
-// import BookingPage from './components/BookingPage';
 import ConfirmedBooking from './components/ConfirmedBooking';
 
 test('Renders the ConfirmBooking heading', () => {
   render(
-    // <BrowserRouter>
       <ConfirmedBooking 
           formData
-          // // Reducer stuff...
-          // availableTimes
-          // dispatch
-
-          // // data filled by user of the app and their setters
-          // date
-          // setDate
-          // time
-          // setTime
-          // guestsNumber
-          // setGuestsNumber
-          // occasion
-          // setOccasion
-          // firstName
-          // setFirstName
-          // lastName
-          // setLastName
-          // email
-          // setEmail
-          // phone
-          // setPhone
-
-          // // is touched data and their setters
-          // dateIsTouched
-          // guestsNumberIsTouched
-          // firstNameIsTouched
-          // lastNameIsTouched
-          // emailIsTouched
-          // phoneIsTouched
-          // setDateIsTouched
-          // setGuestsNumberIsTouched
-          // setFirstNameIsTouched
-          // setLastNameIsTouched
-          // setEmailIsTouched
-          // setPhoneIsTouched
-
-          // // the data that will be summitted
-          // formData
-          // setFormData
-
-          // // submit function and response
-          // submitForm
-          // response
-
-          // // clearing the form after a successfull submit
-          // clearForm
       />
-    // </BrowserRouter>
   );
   const headingElement = screen.getByText("Your reservation is confirmed!, an email was sent to");
   expect(headingElement).toBeInTheDocument();
